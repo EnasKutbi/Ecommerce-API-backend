bfa49816373adf8ef0bc3c9cef76ff62c8f73dec
EnasKutbi <enaskutbi@gmail.com>
2024-04-23 11:52:38 +0300

    create customers table

--- database.sql
+++ database.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE ecommerce;
+
+CREATE TABLE Customers(
+    customer_id SERIAL PRIMARY KEY,
+    customer_first_name VARCHAR(50) NOT NULL,
+    customer_last_name VARCHAR(50),
+    customer_email VARCHAR(50) UNIQUE NOT NULL,
+    customer_password VARCHAR(50) NOT NULL,
+    customer_address VARCHAR(50) NOT NULL
+);
