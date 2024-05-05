ff09f1dc0a6e8f24b9d90788dadd2f11b44d97d3
EnasKutbi <enaskutbi@gmail.com>
2024-05-05 04:09:06 +0300

    user try catch

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -19,63 +19,116 @@ namespace src.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllUsers()
         {
-            var users = await _userService.GetAllUsersService();
-            return Ok(users);
+            try
+            {
+                var users = await _userService.GetAllUsersService();
+                if (users.ToList().Count < 1)
+                {
+                    return NotFound(new ErrorResponse { Success = false, Message = "There is no users to display" });
+                }
+                else
+                {
+                    return Ok(new SuccessResponse<IEnumerable<User>> { Success = true, Message = "all users are returned successfully", data = users });
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred here when tried get all users");
+                return StatusCode(500, new ErrorResponse { Message = ex.Message });
+            }
         }
 
         [HttpGet("{userId}")]
         public async Task<IActionResult> GetUser(string userId)
         {
-            if (!Guid.TryParse(userId, out Guid userIdGuid))
+            try
             {
-                return BadRequest("Invalid user ID Format");
+                if (!Guid.TryParse(userId, out Guid userIdGuid))
+                {
+                    return BadRequest("Invalid user ID Format");
+                }
+                var user = await _userService.GetUserById(userIdGuid);
+                if (user == null)
+                {
+                    return NotFound(new ErrorResponse { Success = false, Message = "No user found" });
+                }
+                else
+                {
+                    return Ok(new SuccessResponse<User> { Success = true, Message = "user is returned successfully", Data = user });
+                }
             }
-            var user = await _userService.GetUserById(userIdGuid);
-            if (user == null)
+            catch (Exception ex)
             {
-                return NotFound();
-            }
-            else
-            {
-                return Ok(user);
+                Console.WriteLine($"An error occurred here when tried get user");
+                return StatusCode(500, new ErrorResponse { Success = false, Message = ex.Message });
             }
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateUser(User newUser)
         {
-            var createdUser = await _userService.CreateUserService(newUser);
-            return CreatedAtAction(nameof(GetUser), new { userId = createdUser.UserId }, createdUser);
+            try
+            {
+                var createdUser = await _userService.CreateUserService(newUser);
+                return CreatedAtAction(nameof(GetUser), new { userId = createdUser.UserId }, createdUser);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"user can not be created");
+                return StatusCode(500, new ErrorResponse { Success = false, Message = ex.Message });
+            }
         }
 
         [HttpPut("{userId}")]
         public async Task<IActionResult> UpdateUser(string userId, User updateUser)
         {
-            if (!Guid.TryParse(userId, out Guid userIdGuid))
+            try
             {
-                return BadRequest("Invalid user ID Format");
+                if (!Guid.TryParse(userId, out Guid userIdGuid))
+                {
+                    return BadRequest("Invalid user ID Format");
+                }
+                var user = await _userService.UpdateUserService(userIdGuid, updateUser);
+                if (user == null)
+                {
+                    return NotFound(new ErrorResponse { Success = false, Message = "No user found" });
+                }
+                else
+                {
+                    return Ok(new SuccessResponse<User> { Success = true, Message = "user is updated successfully", Data = user });
+                }
             }
-            var user = await _userService.UpdateUserService(userIdGuid, updateUser);
-            if (user == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                Console.WriteLine($"user can not be updated");
+                return StatusCode(500, new ErrorResponse { Success = false, Message = ex.Message });
             }
-            return Ok(user);
         }
 
         [HttpDelete("{userId}")]
         public async Task<IActionResult> DeleteUser(string userId)
         {
-            if (!Guid.TryParse(userId, out Guid userIdGuid))
+            try
             {
-                return BadRequest("Invalid user ID Format");
+                if (!Guid.TryParse(userId, out Guid userIdGuid))
+                {
+                    return BadRequest("Invalid user ID Format");
+                }
+                var result = await _userService.DeleteUserService(userIdGuid);
+                if (!result)
+                {
+                    return NotFound(new ErrorResponse { Success = false, Message = "No user found" });
+                }
+                else
+                {
+                    return Ok(new SuccessResponse<User> { Success = true, Message = "user is deleted successfully", Data = user });
+                }
             }
-            var result = await _userService.DeleteUserService(userIdGuid);
-            if (!result)
+            catch (Exception ex)
             {
-                return NotFound();
+                Console.WriteLine($"user can not be deleted");
+                return StatusCode(500, new ErrorResponse { Success = false, Message = ex.Message });
             }
-            return NoContent();
         }
     }
 }
